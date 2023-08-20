d941ae9e79d654cb4d3d02db40456ae6de309c7e
Amasil Rahim Zihad <amasilrahimzihad@gmail.com>
2023-08-20 16:35:30 -0600

    API Key Added Back

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (async () => {
   // Function to fetch movie details from OMDB API
   async function fetchMovieDetails(movieId) {
-    const apiKey = process.env.OMDB_API_KEY;
+    const apiKey = "da76dc6a"; // Your OMDB API key
     const url = `https://www.omdbapi.com/?i=${movieId}&apikey=${apiKey}`;
 
     try {
